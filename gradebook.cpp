4bb3fd8dab98e692eff8729f0e44895e74697360
AML0424 <98441522+AML0424@users.noreply.github.com>
2023-06-16 17:24:02 -0400

    gradebook.cpp base set up
    I created the functions which I know that we will need to use, however I suspect we may need to add some more

--- gradebook.cpp
+++ gradebook.cpp
@@ -1,1 +1,38 @@
+#include "gradebook.h"
 
+
+GradeBook::GradeBook(std::vector<std::vector<std::pair<std::string,int>>>){
+
+}
+
+GradeBook::~GradeBook(){
+
+}
+
+int GradeBook::getCatSize(int category){
+
+}
+
+void GradeBook::FinalAverage(){
+
+}
+
+void GradeBook::CategoryAverage(int category){
+
+}
+
+void GradeBook::addAssignment(int category,int grade){
+
+}
+
+void GradeBook::removeAssignment(int category,int Assignnum){
+
+}
+
+void GradeBook::changeAssignment(int category,int Assignnum, int grade){
+
+}
+
+void GradeBook::printGradebook(){
+
+}
