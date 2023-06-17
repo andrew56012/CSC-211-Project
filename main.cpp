c77e46aa62fabe895361f9d01ef937bd85294e35
AML0424 <98441522+AML0424@users.noreply.github.com>
2023-06-17 15:36:12 -0400

    TXT collection update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,66 @@
 #include "gradebook.h"
 
 int main() {
+    //get info from gradebook
     std::fstream txt;
     txt.open("gradebook.txt",std::fstream::in | std::fstream::out);
 
     std::vector<std::vector<std::pair<std::string,int>>> inputs;
+    std::vector<std::pair<std::string,int>> step1;
+    std::vector<std::string> given;
+
+    std::string enter;
+    int count = 0;
+    //put everting into a base vector
+    while(std::getline(txt,enter)){
+        given.push_back(enter);
+    }
+
+    //get student name
+    std::string name = given[0];
+    name.erase(name.begin(),name.begin()+14);
+
+    //delete unnecessary information from base (name, class name, current grade and spaces in between) (Leaves the graded categories)
+    given.erase(given.begin(),given.begin()+4);
+    given.erase(given.begin()+5);
+    given.erase(given.begin()+3);
+    given.erase(given.begin()+1);
+
+    //delete unnecessary info from individual vectors left(name of category and initial spaces)
+    given[0].erase(given[0].begin(),given[0].begin()+10);
+    given[1].erase(given[1].begin(),given[1].begin()+14);
+    given[2].erase(given[2].begin(),given[2].begin()+6);
+    given[3].erase(given[3].begin(),given[3].begin()+12);
+
+    std::pair<std::string,int> pair;
+
+    //takes the info from the base vector and converts it into pairs of info with the assignment name and grade
+    for(int i = 0; i < given.size(); i++){
+
+        std::stringstream convert(given[i]);
+
+        while(convert >> enter){
+            if(count == 0){
+                pair.first = enter;
+                count++;
+            }
+            else{
+                pair.second = std::stoi(enter);
+                step1.push_back(pair);
+                count--;
+            }
+        }
+
+        inputs.push_back(step1);
+        step1.clear();
+
+    }
+
+    //Output of above statement will look like (the
+    //P1 0 P2 0
+    //A1 0 A2 0 A3 0 A4 0
+    //L1 0 L2 0 L3 0 L4 0 L5 0 L6 0 L7 0 L8 0
+    //FE 0
 
     bool fin = false;
     int selected = 0;
@@ -66,7 +122,12 @@ int main() {
                 }
             }
 
-            Gbook.addAssignment(cat, grade);
+            //get name
+            std::string aname;
+            std::cout << "Please enter the assignment name.\n";
+            std::cin >> aname;
+
+            Gbook.addAssignment(cat, grade, aname);
 
         }
 
@@ -158,4 +219,3 @@ int main() {
     std::cout << "Thank you for using our gradebook editor!\n";
 
 }
-
